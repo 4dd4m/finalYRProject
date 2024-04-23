42e2746ac475b3b2d09f58a787ddb2eab845dfc7
Adam Torok <torok-a@ulster.ac.uk>
2024-04-23 03:43:09 +0100

    ok

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -5,7 +5,8 @@ import json
 import string
 import bcrypt
 from flask import Flask, jsonify, request, make_response
- # import jwt
+import jwt
+from jwt import encode, decode
 from pymongo import MongoClient
 from bson import ObjectId
 from pymongo import aggregation
@@ -26,44 +27,39 @@ staff = db.staffCollection
 #Add secret key for jwt token
 
 #Functions relating to back-end authentication have been commented out due to issue regarding the JWT Token- Issue spoken about in detail with Adrian and code appears to be correct although not functional
+app.config['SECRET_KEY'] = 'mysecret'
 
-# Set the secret key for JWT
-# app.config['SECRET_KEY'] = 'mysecret'
+def jwt_required(func):
+    @wraps(func)
+    def jwt_required_wrapper(*args, **kwargs):
+        token = None
+        data = None
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        if not token:
+            return jsonify( { 'message' : 'Token is missing' }), 401
+        try:
+            data = decode(token, app.config['SECRET_KEY'], algorithms = 'HS256')
+            print("Decoded token data:", data)  # Print decoded token data for debugging
+        except:
+            return jsonify( { 'message' : 'My token is invalid' }), 401
+        return func(*args, **kwargs)
+    return jwt_required_wrapper
 
-# from jwt import encode, decode
+# Login endpoint
+@app.route('/api/v1.0/login', methods=['POST'])
+def login():
+    auth = request.authorization
+    if not auth or not auth.username or not auth.password:
+        return make_response(jsonify({'message': 'Invalid username or password'}), 401)
 
-# def jwt_required(func):
-#     @wraps(func)
-#     def jwt_required_wrapper(*args, **kwargs):
-#         token = None
-#         data = None
-#         if 'x-access-token' in request.headers:
-#             token = request.headers['x-access-token']
-#         if not token:
-#             return jsonify( { 'message' : 'Token is missing' }), 401
-#         try:
-#             data = jwt.decode(token, app.config['SECRET_KEY'], algorithms = 'HS256')
-#             print("Decoded token data:", data)  # Print decoded token data for debugging
-#         except:
-#             return jsonify( { 'message' : 'My token is invalid' }), 401
-#         return func(*args, **kwargs)
-#     return jwt_required_wrapper
+    user = staff.find_one({'username': auth.username})
 
-# # Login endpoint
-# @app.route('/api/v1.0/login', methods=['POST'])
-# def login():
-#     auth = request.authorization
-#     if not auth or not auth.username or not auth.password:
-#         return make_response(jsonify({'message': 'Invalid username or password'}), 401)
+    if not user or not bcrypt.checkpw(auth.password.encode('utf-8'), user['password']):
+        return make_response(jsonify({'message': 'Invalid username or password'}), 401)
 
-#     user = staff.find_one({'username': auth.username})
-
-#     if not user or not bcrypt.checkpw(auth.password.encode('utf-8'), user['password']):
-#         return make_response(jsonify({'message': 'Invalid username or password'}), 401)
-
-#     token = jwt.encode({'username': auth.username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
-#     return jsonify({'token': token.decode('UTF-8')}), 200
-
+    token = encode({'username': auth.username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+    return jsonify({'token': token}), 200
 
 
 @app.route("/api/v1.0/players", methods=["GET"])
@@ -85,7 +81,7 @@ def show_all_players():
 
     return make_response(jsonify(players_data), 200)
 
-# Function to retrieve a single player
+# Function to retrieve a single player by their ID
 @app.route("/api/v1.0/players/<string:id>", methods=["GET"])
 def show_one_player(id):
     if len(id) != 24 or not all(c in string.hexdigits for c in id):
@@ -100,23 +96,51 @@ def show_one_player(id):
     else:
         return make_response(jsonify({"error": "Player not found"}), 404)
     
+
+# Get Players in a specific POS
+
+@app.route("/api/v1.0/players/position/<string:position>", methods=["GET"])
+def get_players_by_position(position):
+    players = collection.find({"POS": position})
+    players_data = []
+    for player in players:
+        # Convert ObjectId to string
+        player["_id"] = str(player["_id"])
+        players_data.append(player)
+    if players_data:
+        return make_response(jsonify(players_data), 200)
+    else:
+        return make_response(jsonify({"error": "Players not found for the given position"}), 404)
+    
+@app.route("/api/v1.0/players/name/<string:name>", methods=["GET"])
+def get_player_by_name(name):
+    player = collection.find_one({"Name": name})
+    if player:
+        # Convert ObjectId to string
+        player["_id"] = str(player["_id"])
+        return make_response(jsonify(player), 200)
+    else:
+        return make_response(jsonify({"error": "Player not found"}), 404)
+
+
 # Function to add a new player
 @app.route("/api/v1.0/players", methods=["POST"])
 def add_new_player():
     # Check if all required fields are present in the request
-    required_fields = ["Team", "Name", "Age", "POS", "App", "Goal Involvements", "Clean Sheets", "Yellows", "Reds", "Mins"]
+    print(request.json)
+    required_fields = ["team", "name", "age", "pos", "app",  "goal", "clean", "yellows", "reds", "mins"]
     if all(field in request.json for field in required_fields):
         new_player = {
-            "Team": request.json["Team"],
-            "Name": request.json["Name"],
-            "Age": int(request.json["Age"]),
-            "POS": request.json["POS"],
-            "App": int(request.json["App"]),
-            "Goal Involvements": int(request.json["Goal Involvements"]),
-            "Clean Sheets": int(request.json["Clean Sheets"]),
-            "Yellows": int(request.json["Yellows"]),
-            "Reds": int(request.json["Reds"]),
-            "Mins": int(request.json["Mins"]),
+            "Team": request.json["team"],
+            "Name": request.json["name"],
+            "Age": int(request.json["age"]),
+            "POS": request.json["pos"],
+            "App": int(request.json["app"]),
+            "Goal Involvements": int(request.json["goal"]),
+            "Clean Sheets": int(request.json["clean"]),
+            "Yellows": int(request.json["yellows"]),
+            "Reds": int(request.json["reds"]),
+            "Mins": int(request.json["mins"]),
             # Include other fields as needed
         }
 
@@ -128,6 +152,7 @@ def add_new_player():
 
         # Convert ObjectId to string
         inserted_player["_id"] = str(inserted_player["_id"])
+        print(inserted_player["_id"])
 
         # Return response with the newly added player
         return make_response(jsonify(inserted_player), 201)
@@ -148,11 +173,53 @@ def delete_player(id):
 
     if result.deleted_count == 1:
         #Check no of players deleted is 1
-        return make_response(jsonify({"message" : f"Player with ID {id} has been deleted"}), 204)
+        return make_response(jsonify({"message" : f"Player with ID {id} has been deleted"}), 200)
     else:
         return make_response(jsonify({"error" : f"Player with ID {id} was not found"}), 404)
 
+#Edit a Players Full Profile
+#Do this using x-www-urlencoded in Postamn For Data
+@app.route("/api/v1.0/players/<string:id>", methods=["PUT"])
+def edit_player(id):
+
+    # Check if ID is valid
+    if len(id) != 24 or not all(c in '0123456789abcdefABCDEF' for c in id):
+        return make_response(jsonify({"error": "Invalid Player ID"}), 404)
+
+    print("Received Form Data: ", request.form)
     
+    # Check if all required fields are present in the request
+    required_fields = ["Team", "Name", "Age", "POS", "App", "Goal Involvements", "Clean Sheets", "Yellows", "Reds", "Mins"]
+    if all(field in request.form for field in required_fields):
+        # Update player details
+        result = collection.update_one(
+            {"_id": ObjectId(id)},
+            {
+                "$set": {
+                    "Team": request.form["Team"],
+                    "Name": request.form["Name"],
+                    "Age": int(request.form["Age"]),
+                    "POS": request.form["POS"],
+                    "App": int(request.form["App"]),
+                    "Goal Involvements": int(request.form["Goal Involvements"]),
+                    "Clean Sheets": int(request.form["Clean Sheets"]),
+                    "Yellows": int(request.form["Yellows"]),
+                    "Reds": int(request.form["Reds"]),
+                    "Mins": int(request.form["Mins"]),
+                    # Add other fields to update as needed
+                }
+            }      
+        )
+
+        # Check if the update was successful
+        if result.matched_count == 1:
+            # Provide link to the edited player's details
+            edit_player_link = f"http://127.0.0.1:5000/api/v1.0/players/{id}"
+            return make_response(jsonify({"url": edit_player_link}), 200)
+        else:
+            return make_response(jsonify({"error": "Invalid Player ID"}), 404)
+    else:
+        return make_response(jsonify({"error": "Missing Form Data"}), 400)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ FrontEnd/src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule, OnInit } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GetPlayerComponent } from './get-player/get-player.component';
+import { MenuItem } from 'primeng/api/menuitem';
+import { MenubarModule } from 'primeng/menubar';
+import { HttpClientModule } from '@angular/common/http';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { ViewPlayerComponent } from './view-player/view-player.component';
+import { EditPlayerComponent } from './edit-player/edit-player.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AddPlayerComponent } from './add-player/add-player.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GetPlayerComponent,
+    ViewPlayerComponent,
+    EditPlayerComponent,
+    AddPlayerComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    MenubarModule,
+    HttpClientModule,
+    TableModule,
+    ButtonModule,
+    InputTextModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+
+export class AppModule {
+}
